91776a9a4f683255c9ac312aba5566c7213b591e
Pablo Narvaez <narvaez.pablog@gmail.com>
2023-07-12 22:18:38 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,27 @@
+//NOMBRE Y APELLIDO
+let nombreyapellidoIngresado = prompt("Ingrese su Nombre y Apellido");
+console.log(nombreyapellidoIngresado);
+console.log(typeof nombreyapellidoIngresado);
+
+//LEGAJO DEL CLIENTE
+let intentos = 3;
+let numeroDeCliente = Number(prompt("Ingrese su número de Legajo Mayor a 1750"));
+console.log(numeroDeCliente);
+console.log(typeof numeroDeCliente);
+
+while (numeroDeCliente < 1750 && intentos > 1) {
+    console.log("Número de Legajo Inexistente, por favor intente de nuevo!");
+    numeroDeCliente = Number(prompt("Ingrese su número de Legajo Mayor o igual a 1750"));
+    intentos--;
+}
+
+if (numeroDeCliente >= 1750) {
+    console.log("Puede Loguearse");
+    document.write("Bienvenido de nuevo " + nombreyapellidoIngresado);
+} else {
+    console.log("Número de Legajo Inexistente. Has alcanzado el límite maximo de intentos. Contacta al administrador.");
+    document.write("Número de Legajo Inexistente. Has alcanzado el límite maximo de intentos. Contacta al administrador.");
+}
+
+/*consigna 3*/
+
